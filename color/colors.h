c0a2f2c4e72e72f5d01f7120f6fb8268631c0cfb
James Pack <jgrimm@gmail.com>
2022-03-09 12:50:50 -0500

    Add helper creation methods for colors

--- color/colors.h
+++ color/colors.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits>
+
 #include "color/color.pb.h"
 
 namespace pack::color {
@@ -10,6 +12,34 @@ class Colors final {
   static Rgba as_floats(const Rgba& color);
   static Rgba as_ints(const Rgba& color);
   static Rgba as_uint32s(const Rgba& color);
+
+  static Rgba create(float red, float green, float blue, float alpha = 1.f) {
+    Rgba result{};
+    result.mutable_float_values()->set_red(red);
+    result.mutable_float_values()->set_green(green);
+    result.mutable_float_values()->set_blue(blue);
+    result.mutable_float_values()->set_alpha(alpha);
+    return result;
+  }
+
+  static Rgba create(int red, int green, int blue, int alpha = 255) {
+    Rgba result{};
+    result.mutable_int_values()->set_red(red);
+    result.mutable_int_values()->set_green(green);
+    result.mutable_int_values()->set_blue(blue);
+    result.mutable_int_values()->set_alpha(alpha);
+    return result;
+  }
+
+  static Rgba create(uint32_t red, uint32_t green, uint32_t blue,
+                     uint32_t alpha = std::numeric_limits<uint32_t>::max()) {
+    Rgba result{};
+    result.mutable_uint32_values()->set_red(red);
+    result.mutable_uint32_values()->set_green(green);
+    result.mutable_uint32_values()->set_blue(blue);
+    result.mutable_uint32_values()->set_alpha(alpha);
+    return result;
+  }
 };
 
 }  // namespace pack::color
