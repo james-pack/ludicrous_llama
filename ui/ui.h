f172428782aac046a13a583db9e925542904b791
James Pack <jgrimm@gmail.com>
2022-03-10 14:18:57 -0500

    Move glsl version determination to the UI package

--- ui/ui.h
+++ ui/ui.h
@@ -10,3 +10,32 @@
 // End of order dependent headers.
 
 // clang-format on
+
+namespace pack::ui {
+
+inline const char* determine_glsl_version() {
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+  // GL ES 2.0 + GLSL 100
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+  const char* glsl_version = "#version 100";
+#elif defined(__APPLE__)
+  // GL 3.2 + GLSL 150
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+  const char* glsl_version = "#version 150";
+#else
+  // GL 3.3 + GLSL 130
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+  const char* glsl_version = "#version 130";
+#endif
+  return glsl_version;
+}
+
+}  // namespace pack::ui
