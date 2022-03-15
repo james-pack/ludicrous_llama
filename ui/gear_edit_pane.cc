5e5833c722901fb2eaa26781ada2795b688f1401
James Pack <jgrimm@gmail.com>
2022-03-15 13:16:44 -0400

    Add UI elements for configuring gear parameters

--- ui/gear_edit_pane.cc
+++ ui/gear_edit_pane.cc
@@ -8,6 +8,7 @@
 #include "ui/model/animate.h"
 #include "ui/model/gear.h"
 #include "ui/model/position.h"
+#include "ui/render.h"
 
 namespace pack::ui {
 
@@ -31,12 +32,33 @@ void GearEditPane::render() {
     auto models = registry.view<Gear, Position, Orientation>();
     models.each([&registry](const auto entity, Gear& gear, Position& position, Orientation& orientation) {
       bool position_was_changed{false};
+      bool component_was_changed{false};
       ImGui::PushID(gear.id.c_str());
       if (ImGui::TreeNodeEx(gear.id.c_str(), ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_Bullet)) {
-        position_was_changed = ImGui::SliderFloat3("Position", position.position, -50.f, 50.f, "%.2f");
+        position_was_changed =
+            ImGui::SliderFloat3("Position", position.position, -50.f, 50.f, "%.2f") || position_was_changed;
+        component_was_changed =
+            ImGui::SliderFloat("Inner radius", &gear.inner_radius, 0.f, 25.f, "%.2f") || component_was_changed;
+        component_was_changed =
+            ImGui::SliderFloat("Outer radius", &gear.outer_radius, 0.f, 25.f, "%.2f") || component_was_changed;
+        component_was_changed = ImGui::SliderFloat("Width", &gear.width, 0.f, 25.f, "%.2f") || component_was_changed;
+        component_was_changed = ImGui::SliderInt("Teeth", &gear.teeth, 2, 1000) || component_was_changed;
+        component_was_changed =
+            ImGui::SliderFloat("Tooth depth", &gear.tooth_depth, 0.f, 25.f, "%.2f") || component_was_changed;
+        component_was_changed =
+            ImGui::SliderFloat("Angular speed", &gear.angle_coefficient, 0.f, 1000.f, "%.2f") || component_was_changed;
+        component_was_changed =
+            ImGui::SliderFloat("Angular phase", &gear.phase, 0.f, 1000.f, "%.2f") || component_was_changed;
         ImGui::TreePop();
       }
       ImGui::PopID();
+      if (component_was_changed) {
+        // TODO(james): Clean up unused draw list. Perhaps by making the Render element manage creation and
+        // destruction of these draw lists?
+        int draw_list_id = build_gear(gear);
+        registry.replace<Render>(entity, construct_draw_list_renderer(draw_list_id));
+        registry.replace<Gear>(entity, gear);
+      }
       if (position_was_changed) {
         registry.replace<Position>(entity, position);
       }
