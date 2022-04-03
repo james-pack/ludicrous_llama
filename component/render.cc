fe3f561e288301b9ee38085a233bb99b3c0e0fef
James Pack <jgrimm@gmail.com>
2022-04-03 08:12:46 -0400

    Add PrimitiveRender to build

--- component/render.cc
+++ component/render.cc
@@ -5,18 +5,20 @@
 
 namespace pack::component {
 
-bool PrimitiveRenderer::render(const ParameterBinding::Set& bindings) {
-  bool can_render{is_valid_ && previous_bindings_ == bindings};
-  if (!can_render) {
-    if (primitive_->can_generate(bindings)) {
-      previous_bindings = bindings;
-      draw_func_ = primitive_->generate(previous_bindings);
-      is_valid_ = true;
-      can_render = true;
+void PrimitiveRenderer::render(const ParameterBinding::Set& bindings) {
+  if (primitive_ != nullptr) {
+    bool can_render{is_valid_ && previous_bindings_ == bindings};
+    if (!can_render) {
+      if (primitive_->can_generate(bindings)) {
+        previous_bindings_ = bindings;
+        draw_func_ = primitive_->generate(previous_bindings_);
+        is_valid_ = true;
+        can_render = true;
+      }
     }
-  }
-  if (can_render) {
-    draw_func_();
+    if (can_render) {
+      draw_func_();
+    }
   }
 }
 
